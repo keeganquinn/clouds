aac1b1a85d2bfdd15e5c6617b9444c6eaa3d39c2
Keegan Quinn <keeganquinn@gmail.com>
2010-11-19 12:12:23 -0800

    Added additional User attributes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
          :token_authenticatable, :confirmable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  validates_length_of :login, :within => 3..40
+  validates_uniqueness_of :login
+  validates_format_of :login, :with => /\A[A-Za-z0-9]+\Z/
+
+  validates_length_of :name, :maximum => 128
+  validates_length_of :city, :maximum => 128
+  validates_length_of :country, :maximum => 2
 end
