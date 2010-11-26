798ccb9c141b1f368866ffddff90c2d4f34102b7
Keegan Quinn <keeganquinn@gmail.com>
2010-11-26 03:25:04 -0800

    Complete behavior specifications for routing.

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -2,34 +2,36 @@ require "spec_helper"
 
 describe PostsController do
   describe "routing" do
-
-    it "recognizes and generates #index" do
+    it "recognizes and generates site-wide #index" do
       { :get => "/posts" }.should route_to(:controller => "posts", :action => "index")
     end
 
+    it "recognizes and generates per-user #index" do
+      { :get => "/users/someone/posts" }.should route_to(:controller => "posts", :action => "index", :user_id => "someone")
+    end
+
     it "recognizes and generates #new" do
-      { :get => "/posts/new" }.should route_to(:controller => "posts", :action => "new")
+      { :get => "/users/someone/posts/new" }.should route_to(:controller => "posts", :action => "new", :user_id => "someone")
     end
 
     it "recognizes and generates #show" do
-      { :get => "/posts/1" }.should route_to(:controller => "posts", :action => "show", :id => "1")
+      { :get => "/users/someone/posts/1" }.should route_to(:controller => "posts", :action => "show", :id => "1", :user_id => "someone")
     end
 
     it "recognizes and generates #edit" do
-      { :get => "/posts/1/edit" }.should route_to(:controller => "posts", :action => "edit", :id => "1")
+      { :get => "/users/someone/posts/1/edit" }.should route_to(:controller => "posts", :action => "edit", :id => "1", :user_id => "someone")
     end
 
     it "recognizes and generates #create" do
-      { :post => "/posts" }.should route_to(:controller => "posts", :action => "create")
+      { :post => "/users/someone/posts" }.should route_to(:controller => "posts", :action => "create", :user_id => "someone")
     end
 
     it "recognizes and generates #update" do
-      { :put => "/posts/1" }.should route_to(:controller => "posts", :action => "update", :id => "1")
+      { :put => "/users/someone/posts/1" }.should route_to(:controller => "posts", :action => "update", :id => "1", :user_id => "someone")
     end
 
     it "recognizes and generates #destroy" do
-      { :delete => "/posts/1" }.should route_to(:controller => "posts", :action => "destroy", :id => "1")
+      { :delete => "/users/someone/posts/1" }.should route_to(:controller => "posts", :action => "destroy", :id => "1", :user_id => "someone")
     end
-
   end
 end
---
+++ spec/routing/welcome_routing_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+describe WelcomeController do
+  describe 'routing' do
+    it "recognizes and generates #index" do
+      { :get => "/" }.should route_to(:controller => "welcome", :action => "index")
+    end
+  end
+end
