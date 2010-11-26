16ce12c9c121eee49637ff0fb9c7bfc7421560c0
Keegan Quinn <keeganquinn@gmail.com>
2010-11-26 03:06:23 -0800

    Added behavior specifications for the Post and User models.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,52 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = Post.new
+  end
+
+  it 'should be invalid if code is too long' do
+    @post.subject = 'Model Test'
+    @post.body = 'Testing.'
+    @post.should be_valid
+
+    @post.code = '0123456789' * 30
+    @post.should_not be_valid
+  end
+
+  it 'should be invalid if code is not unique' do
+    @post.subject = 'Test 1'
+    @post.body = 'Test one.'
+    @post.save.should be_true
+
+    @duplicate_post = Post.new
+    @duplicate_post.subject = 'Test 1'
+    @duplicate_post.body = 'Test one.'
+    @duplicate_post.should_not be_valid
+  end
+
+  it 'should be invalid if code contains unwanted characters' do
+    @post.subject = 'Model Test'
+    @post.body = 'Testing.'
+    @post.should be_valid
+
+    @post.code = 'bad!@$'
+    @post.should_not be_valid
+  end
+
+  it 'should be invalid without a subject and body' do
+    @post.should_not be_valid
+
+    @post.subject = 'Model Test'
+    @post.body = 'Testing.'
+    @post.should be_valid
+  end
+
+  it 'should generate a code from the subject if none is provided' do
+    @post.subject = 'Model Test'
+    @post.body = 'Testing.'
+    @post.should be_valid
+
+    @post.code.should match(/model-test/)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,67 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new
+
+    @user.email = 'test@test.com'
+    @user.password = 'some_password'
+    @user.password_confirmation = 'some_password'
+  end
+
+  it 'should be invalid if username is too short' do
+    @user.should be_valid
+
+    @user.username = 'ab'
+    @user.should_not be_valid
+  end
+
+  it 'should be invalid if username is too long' do
+    @user.should be_valid
+
+    @user.username = '0123456789' * 5
+    @user.should_not be_valid
+  end
+
+  it 'should be invalid if username is not unique' do
+    @user.username = 'test'
+    @user.save.should be_true
+
+    @duplicate_user = User.new
+    @duplicate_user.email = 'test2@test.com'
+    @duplicate_user.password = 'another_password'
+    @duplicate_user.password_confirmation = 'another_password'
+    @duplicate_user.should be_valid
+
+    @duplicate_user.username = 'test'
+    @duplicate_user.should_not be_valid
+  end
+
+  it 'should be invalid if username contains unwanted characters' do
+    @user.should be_valid
+
+    @user.username = 'bad!@$'
+    @user.should_not be_valid
+  end
+
+  it 'should be invalid if name is too long' do
+    @user.should be_valid
+
+    @user.name = '0123456789' * 13
+    @user.should_not be_valid
+  end
+
+  it 'should be invalid if city is too long' do
+    @user.should be_valid
+
+    @user.city = '0123456789' * 13
+    @user.should_not be_valid
+  end
+
+  it 'should be invalid if country is too long' do
+    @user.should be_valid
+
+    @user.country = 'ABC'
+    @user.should_not be_valid
+  end
 end
