0a9d8fb08987a75aa46cf0b8c906aa699e0e9dcc
Keegan Quinn <keeganquinn@gmail.com>
2010-11-26 18:28:59 -0800

    Cleaned up PostsController.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, :except => [ :index, :show ]
+
   # GET /posts
   # GET /posts.xml
   def index
@@ -30,12 +32,7 @@ class PostsController < ApplicationController
   # GET /posts/new
   # GET /posts/new.xml
   def new
-    redirect_to(new_user_session) unless user_signed_in?
-
-    @user = User.find_by_param(params[:user_id])
-    redirect_to(new_user_post(current_user)) unless @user.id == current_user.id
-
-    @post = @user.posts.new
+    @post = current_user.posts.new
 
     unless params[:in_reply_to_post_id].blank?
       @post.in_reply_to_post = Post.find(params[:in_reply_to_post_id])
@@ -50,31 +47,28 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
-    redirect_to(new_user_session) unless user_signed_in?
-
-    @user = User.find_by_param(params[:user_id])
-    redirect_to(new_user_post(current_user)) unless @user.id == current_user.id
-
-    @post = @user.posts.find_by_param(params[:id])
+    @post = current_user.posts.find_by_param(params[:id])
   end
 
   # POST /posts
   # POST /posts.xml
   def create
-    redirect_to(new_user_session) unless user_signed_in?
-
-    @user = User.find_by_param(params[:user_id])
-    redirect_to(new_user_post(current_user)) unless @user.id == current_user.id
-
-    @post = @user.posts.new(params[:post])
+    @post = current_user.posts.new(params[:post])
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to(user_post_path(@user, @post), :notice => 'Post was successfully created.') }
-        format.xml  { render :xml => @post, :status => :created, :location => @post }
+        format.html {
+          redirect_to(user_post_path(current_user, @post),
+                      :notice => 'Post was successfully created.')
+        }
+        format.xml  {
+          render :xml => @post, :status => :created, :location => @post
+        }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+        format.xml  {
+          render :xml => @post.errors, :status => :unprocessable_entity
+        }
       end
     end
   end
@@ -82,20 +76,20 @@ class PostsController < ApplicationController
   # PUT /posts/1
   # PUT /posts/1.xml
   def update
-    redirect_to(new_user_session) unless user_signed_in?
-
-    @user = User.find_by_param(params[:user_id])
-    redirect_to(new_user_post(current_user)) unless @user.id == current_user.id
-
-    @post = @user.posts.find_by_param(params[:id])
+    @post = current_user.posts.find_by_param(params[:id])
 
     respond_to do |format|
       if @post.update_attributes(params[:post])
-        format.html { redirect_to(user_post_path(@user, @post), :notice => 'Post was successfully updated.') }
+        format.html {
+          redirect_to(user_post_path(current_user, @post),
+                      :notice => 'Post was successfully updated.')
+        }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
+        format.xml  {
+          render :xml => @post.errors, :status => :unprocessable_entity
+        }
       end
     end
   end
@@ -103,16 +97,11 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.xml
   def destroy
-    redirect_to(new_user_session) unless user_signed_in?
-
-    @user = User.find_by_param(params[:user_id])
-    redirect_to(new_user_post(current_user)) unless @user.id == current_user.id
-
-    @post = @user.posts.find_by_param(params[:id])
+    @post = current_user.posts.find_by_param(params[:id])
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to(user_posts_url(@user)) }
+      format.html { redirect_to(user_posts_path(current_user)) }
       format.xml  { head :ok }
     end
   end
