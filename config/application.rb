5cb61c876128fbe817ebd1206905de74fa23e39e
Keegan Quinn <keeganquinn@gmail.com>
2011-03-24 17:26:33 -0700

    Stop trying to use Factory Girl for fixtures; it never worked.

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,6 @@ module Clouds
     config.generators do |g|
       g.template_engine :haml
       g.test_framework :rspec, :fixture => true, :views => false
-      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
     end
   end
 end
