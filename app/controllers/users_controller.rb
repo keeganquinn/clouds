bbde2aafe273d415c928c0eeb8243b93fbc907fb
Keegan Quinn <keeganquinn@gmail.com>
2011-09-08 20:07:33 -0700

    Updated route example comments.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,16 @@ class UsersController < ApplicationController
   respond_to :html, :json, :xml
 
   # GET /users
+  # GET /users.json
   # GET /users.xml
   def index
     @users = User.with_profile.paginate(:page => params[:page])
     respond_with(@users)
   end
 
-  # GET /users/1
-  # GET /users/1.xml
+  # GET /users/id
+  # GET /users/id.json
+  # GET /users/id.xml
   def show
     @user = User.find_by_param(params[:id])
     respond_with(@user)
