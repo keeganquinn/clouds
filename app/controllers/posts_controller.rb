91b6e6056d476ef6f93e8d5d06e2126987dc9bde
Keegan Quinn <keeganquinn@gmail.com>
2011-09-08 20:16:18 -0700

    Use respond_with for concise multi-format resource access, and update route example comments.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,37 +1,36 @@
 class PostsController < ApplicationController
   before_filter :authenticate_user!, :except => [ :index, :show ]
+  respond_to :html, :json, :xml
 
   # GET /posts
+  # GET /posts.json
   # GET /posts.xml
+  # GET /users/user_id/posts
+  # GET /users/user_id/posts.json
+  # GET /users/user_id/posts.xml
   def index
-    unless params[:user_id].blank?
+    if params[:user_id].blank?
+      @posts = Post.top.paginate(:page => params[:page])
+    else
       @user = User.find_by_param(params[:user_id])
       @posts = @user.posts.top.paginate(:page => params[:page])
-    else
-      @posts = Post.top.paginate(:page => params[:page])
     end
 
-    respond_to do |format|
-      format.html # index.html.haml
-      format.xml  { render :xml => @posts }
-      format.json { render :json => @posts }
-    end
+    respond_with(@posts)
   end
 
-  # GET /posts/1
-  # GET /posts/1.xml
+  # GET /users/user_id/posts/id
+  # GET /users/user_id/posts/id.json
+  # GET /users/user_id/posts/id.xml
   def show
     user = User.find_by_param(params[:user_id])
     @post = user.posts.find_by_param(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.haml
-      format.xml  { render :xml => @post }
-      format.json { render :json => @post }
-    end
+    respond_with(@post)
   end
 
   # GET /posts/new
+  # GET /posts/new.json
   # GET /posts/new.xml
   def new
     @post = current_user.posts.new
@@ -41,82 +40,53 @@ class PostsController < ApplicationController
       @post.subject ||= "Re: #{@post.in_reply_to_post.subject}"
     end
 
-    respond_to do |format|
-      format.html # new.html.haml
-      format.xml  { render :xml => @post }
-      format.json { render :json => @post }
-    end
+    respond_with(@post)
   end
 
-  # GET /posts/1/edit
+  # GET /users/user_id/posts/id/edit
   def edit
     @post = current_user.posts.find_by_param(params[:id])
   end
 
   # POST /posts
+  # POST /posts.json
   # POST /posts.xml
   def create
     @post = current_user.posts.new(params[:post])
 
-    respond_to do |format|
-      if @post.save
-        format.html {
-          redirect_to(user_post_path(current_user, @post),
-                      :notice => 'Post was successfully created.')
-        }
-        format.xml  {
-          render :xml => @post, :status => :created, :location => @post
-        }
-        format.json {
-          render :json => @post, :status => :created, :location => @post
-        }
-      else
+    if @post.save
+      respond_with(@post, :status => :created,
+          :location => user_post_path(current_user, @post))
+    else
+      respond_with(@post.errors, :status => :unprocessable_entity) do |format|
         format.html { render :action => "new" }
-        format.xml  {
-          render :xml => @post.errors, :status => :unprocessable_entity
-        }
-        format.json {
-          render :json => @post.errors, :status => :unprocessable_entity
-        }
       end
     end
   end
 
-  # PUT /posts/1
-  # PUT /posts/1.xml
+  # PUT /users/user_id/posts/id
+  # PUT /users/user_id/posts/id.json
+  # PUT /users/user_id/posts/id.xml
   def update
     @post = current_user.posts.find_by_param(params[:id])
 
-    respond_to do |format|
-      if @post.update_attributes(params[:post])
-        format.html {
-          redirect_to(user_post_path(current_user, @post),
-                      :notice => 'Post was successfully updated.')
-        }
-        format.xml  { head :ok }
-        format.json { head :ok }
-      else
+    if @post.update_attributes(params[:post])
+      respond_with(@post, :head => :ok,
+          :location => user_post_path(current_user, @post))
+    else
+      respond_with(@post.errors, :status => :unprocessable_entity) do |format|
         format.html { render :action => "edit" }
-        format.xml  {
-          render :xml => @post.errors, :status => :unprocessable_entity
-        }
-        format.json {
-          render :json => @post.errors, :status => :unprocessable_entity
-        }
       end
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.xml
+  # DELETE /users/user_id/posts/id
+  # DELETE /users/user_id/posts/id.json
+  # DELETE /users/user_id/posts/id.xml
   def destroy
     @post = current_user.posts.find_by_param(params[:id])
     @post.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(user_posts_path(current_user)) }
-      format.xml  { head :ok }
-      format.json { head :ok }
-    end
+    respond_with(current_user, :head => :ok, :location => user_posts_path(current_user))
   end
 end
