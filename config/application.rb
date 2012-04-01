22c4417fd41350fa929e02e7612c888ea89da7fc
Keegan Quinn <keeganquinn@gmail.com>
2012-04-01 02:31:17 -0700

    Set config.active_record.whitelist_attributes to true, to help avoid security vulnerabilities.

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,9 @@ module Clouds
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
+    # Require model attributes to be whitelisted
+    config.active_record.whitelist_attributes = true
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Pacific Time (US & Canada)'
