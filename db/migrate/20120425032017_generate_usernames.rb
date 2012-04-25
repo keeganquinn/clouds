54f352c95c614f0a33c13ffaec3de3e40f100d52
Keegan Quinn <keeganquinn@gmail.com>
2012-04-24 20:22:53 -0700

    Added migration to generate missing usernames.

---
+++ db/migrate/20120425032017_generate_usernames.rb
@@ -0,0 +1,13 @@
+class GenerateUsernames < ActiveRecord::Migration
+  def up
+    users = User.find_all_by_username(nil)
+
+    users.each do |user|
+      user.username = "user#{user.id}"
+      user.save(false)
+    end
+  end
+
+  def down
+  end
+end
