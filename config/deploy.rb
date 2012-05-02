b212851261e398e0a78750d5c7e321b6a8fe4a64
Keegan Quinn <keeganquinn@gmail.com>
2012-05-02 16:53:07 -0700

    Added Capistrano deployment configuration bits for ThinkingSphinx.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 require 'bundler/capistrano'
+require 'thinking_sphinx/deploy/capistrano'
 
 set :application, "clouds"
 
@@ -21,3 +22,15 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
+before 'deploy:update_code', 'thinking_sphinx:stop'
+after 'deploy:update_code', 'thinking_sphinx:start'
+
+namespace :sphinx do
+  desc "Symlink Sphinx indexes"
+  task :symlink_indexes, roles: :app do
+    run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
+  end
+end
+
+after 'deploy:finalize_update', 'sphinx:symlink_indexes'
